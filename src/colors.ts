5d3347bda6ad398eeaaa6e6e68f73785cc478183
alszczep <al.szczepura@gmail.com>
2024-08-17 01:23:46 +0200

    Handle movement and attacking better

--- src/colors.ts
+++ src/colors.ts
@@ -11,8 +11,8 @@ type ColorVectors = {
 };
 
 export const colorVectors: ColorVectors = {
-  [colorKeys.sky]: [0.5, 0.8, 1],
-  [colorKeys.ground]: [0.2, 0.9, 0.2],
+  [colorKeys.sky]: [0.5, 0.7, 0.9],
+  [colorKeys.ground]: [0.2, 0.5, 0.2],
   [colorKeys.red]: [1, 0, 0],
   [colorKeys.yellow]: [1, 1, 0],
   [colorKeys.blue]: [0, 0, 1],
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { spriteSheetData } from "./assets/spriteSheetData";
 import { colorKeys, colorVectors } from "./colors";
 import { TerrainProgram } from "./programs/TerrainProgram";
 import { CharacterProgram } from "./programs/CharacterProgram";
+import { SPRITE_SIZE_MULTIPLIER } from "./consts";
 
 async function main() {
   const spriteSheet = await loadSpriteSheet();
@@ -41,50 +42,126 @@ async function main() {
   const texCoordsCharacterAttacking =
     spriteSheetData["character-2 1.aseprite"].texCoords;
 
-  const characterData = {
+  const characterData: {
+    x: number;
+    y: number;
+    facing: "left" | "right";
+    swordColor: keyof typeof colorKeys;
+    attackingTime?: number;
+    isAttackButtonPressed: boolean;
+    isMoving: boolean;
+    movingTimeLeft?: number;
+  } = {
     x: 0,
     y: 0,
     facing: "right",
-    isAttacking: false,
     swordColor: colorKeys.red as keyof typeof colorKeys,
+    attackingTime: undefined,
+    isAttackButtonPressed: false,
+    isMoving: false,
+    movingTimeLeft: undefined,
   };
+
+  const move = (direction: "left" | "right") => {
+    characterData.facing = direction;
+    characterData.isMoving = true;
+    characterData.movingTimeLeft = 25;
+  };
+
+  const attack = (
+    color:
+      | typeof colorKeys.red
+      | typeof colorKeys.yellow
+      | typeof colorKeys.blue
+  ) => {
+    if (characterData.attackingTime || characterData.isAttackButtonPressed)
+      return;
+
+    characterData.swordColor = color;
+    characterData.attackingTime = 200;
+    characterData.isAttackButtonPressed = true;
+  };
+
   window.addEventListener("keydown", (e) => {
     switch (e.key) {
       case "d":
-        characterData.x += 5;
-        characterData.facing = "right";
+        move("right");
         break;
       case "a":
-        characterData.x -= 5;
-        characterData.facing = "left";
-        break;
-      case "w":
-        characterData.y -= 5;
-        break;
-      case "s":
-        characterData.y += 5;
+        move("left");
         break;
       case "j":
-        characterData.isAttacking = true;
-        characterData.swordColor = colorKeys.red;
+        attack("red");
         break;
       case "k":
-        characterData.isAttacking = true;
-        characterData.swordColor = colorKeys.yellow;
+        attack("yellow");
         break;
       case "l":
-        characterData.isAttacking = true;
-        characterData.swordColor = colorKeys.blue;
+        attack("blue");
         break;
     }
   });
   window.addEventListener("keyup", (e) => {
-    if (["j", "k", "l"].includes(e.key)) {
-      characterData.isAttacking = false;
+    switch (e.key) {
+      case "d":
+        if (characterData.isMoving && characterData.facing === "right") {
+          characterData.isMoving = false;
+        }
+        break;
+      case "a":
+        if (characterData.isMoving && characterData.facing === "left") {
+          characterData.isMoving = false;
+        }
+        break;
+      case "j":
+        if (characterData.swordColor === colorKeys.red) {
+          characterData.isAttackButtonPressed = false;
+        }
+        break;
+      case "k":
+        if (characterData.swordColor === colorKeys.yellow) {
+          characterData.isAttackButtonPressed = false;
+        }
+        break;
+      case "l":
+        if (characterData.swordColor === colorKeys.blue) {
+          characterData.isAttackButtonPressed = false;
+        }
+        break;
     }
   });
 
-  function drawScene() {
+  let lastFrameTime = 0;
+  function drawScene(frameTime: DOMHighResTimeStamp) {
+    const deltaTime = frameTime - lastFrameTime;
+    lastFrameTime = frameTime;
+
+    if (characterData.movingTimeLeft !== undefined) {
+      if (characterData.facing === "right") {
+        characterData.x += deltaTime / 4;
+      } else {
+        characterData.x -= deltaTime / 4;
+      }
+
+      characterData.movingTimeLeft -= deltaTime;
+
+      if (characterData.movingTimeLeft <= 0) {
+        if (characterData.isMoving) {
+          characterData.movingTimeLeft = 25;
+        } else {
+          characterData.movingTimeLeft = undefined;
+        }
+      }
+    }
+
+    if (characterData.attackingTime !== undefined) {
+      characterData.attackingTime -= deltaTime;
+
+      if (characterData.attackingTime <= 0) {
+        characterData.attackingTime = undefined;
+      }
+    }
+
     // gl.clearColor(0, 0, 0, 0);
     // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
@@ -100,9 +177,23 @@ async function main() {
     });
     terrainProgram.drawTerrain({
       x: 0,
-      y: gl.canvas.height - 100,
+      y: gl.canvas.height - 50,
       w: gl.canvas.width,
-      h: 100,
+      h: 50,
+      color: colorVectors.ground,
+    });
+    terrainProgram.drawTerrain({
+      x: gl.canvas.width / 6,
+      y: gl.canvas.height - 175,
+      w: gl.canvas.width / 6,
+      h: 25,
+      color: colorVectors.ground,
+    });
+    terrainProgram.drawTerrain({
+      x: gl.canvas.width - gl.canvas.width / 3,
+      y: gl.canvas.height - 175,
+      w: gl.canvas.width / 6,
+      h: 25,
       color: colorVectors.ground,
     });
 
@@ -110,19 +201,11 @@ async function main() {
     characterProgram.useProgram();
 
     characterProgram.drawCharacter({
-      x: 0,
-      y: 0,
-      w: spriteSheetData["character-2 0.aseprite"].w,
-      h: spriteSheetData["character-2 0.aseprite"].h,
-      grayOffsetColor: colorVectors[characterData.swordColor],
-      texCoords: characterData.isAttacking
-        ? texCoordsCharacterAttacking
-        : texCoordsCharacterStanding,
-      flipX: characterData.facing === "left",
-    });
-    characterProgram.drawCharacter({
       x: 200,
-      y: 0,
+      y:
+        gl.canvas.height -
+        50 -
+        spriteSheetData["enemy-knight 0.aseprite"].h * SPRITE_SIZE_MULTIPLIER,
       w: spriteSheetData["enemy-knight 0.aseprite"].w,
       h: spriteSheetData["enemy-knight 0.aseprite"].h,
       grayOffsetColor: colorVectors.yellow,
@@ -130,7 +213,10 @@ async function main() {
     });
     characterProgram.drawCharacter({
       x: 400,
-      y: 0,
+      y:
+        gl.canvas.height -
+        50 -
+        spriteSheetData["enemy-knight 0.aseprite"].h * SPRITE_SIZE_MULTIPLIER,
       w: spriteSheetData["enemy-knight 1.aseprite"].w,
       h: spriteSheetData["enemy-knight 1.aseprite"].h,
       grayOffsetColor: colorVectors.blue,
@@ -138,11 +224,29 @@ async function main() {
     });
     characterProgram.drawCharacter({
       x: 600,
-      y: 0,
+      y:
+        gl.canvas.height -
+        50 -
+        spriteSheetData["enemy-knight 0.aseprite"].h * SPRITE_SIZE_MULTIPLIER,
       w: spriteSheetData["enemy-knight 1.aseprite"].w,
       h: spriteSheetData["enemy-knight 1.aseprite"].h,
       grayOffsetColor: colorVectors.red,
       texCoords: spriteSheetData["enemy-knight 1.aseprite"].texCoords,
+    });
+
+    characterProgram.drawCharacter({
+      x: characterData.x,
+      y:
+        gl.canvas.height -
+        50 -
+        spriteSheetData["character-2 0.aseprite"].h * SPRITE_SIZE_MULTIPLIER,
+      w: spriteSheetData["character-2 0.aseprite"].w,
+      h: spriteSheetData["character-2 0.aseprite"].h,
+      grayOffsetColor: colorVectors[characterData.swordColor],
+      texCoords: characterData.attackingTime
+        ? texCoordsCharacterAttacking
+        : texCoordsCharacterStanding,
+      flipX: characterData.facing === "left",
     });
 
     requestAnimationFrame(drawScene);
