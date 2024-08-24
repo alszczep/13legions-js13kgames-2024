d6dd6361fffcf8a8624d4cb7fe0da44eb8dc617c
alszczep <al.szczepura@gmail.com>
2024-08-24 13:25:56 +0200

    Add game difficulty picker

--- src/views/mainMenuView.ts
+++ src/views/mainMenuView.ts
@@ -5,11 +5,29 @@ import { GameView } from "./gameView";
 export async function mainMenuView(startGame: (gv: GameView) => void) {
   const container = document.getElementById("a")! as HTMLDivElement;
 
-  const startButton = document.createElement("button");
-  startButton.innerText = "Start";
-  container.appendChild(startButton);
-  startButton.onclick = () =>
+  const westButton = container.querySelector(".t.w")! as HTMLDivElement;
+  const southButton = container.querySelector(".t.s")! as HTMLDivElement;
+  const eastButton = container.querySelector(".t.e")! as HTMLDivElement;
+  const northButton = container.querySelector(".t.n")! as HTMLDivElement;
+
+  westButton.addEventListener("click", () => {
     startGame({
       stageModifiers: stageModifiersDefinitions[GameDifficulty.Easy],
     });
+  });
+  southButton.addEventListener("click", () => {
+    startGame({
+      stageModifiers: stageModifiersDefinitions[GameDifficulty.Normal],
+    });
+  });
+  eastButton.addEventListener("click", () => {
+    startGame({
+      stageModifiers: stageModifiersDefinitions[GameDifficulty.Hard],
+    });
+  });
+  northButton.addEventListener("click", () => {
+    startGame({
+      stageModifiers: stageModifiersDefinitions[GameDifficulty.Nightmare],
+    });
+  });
 }
