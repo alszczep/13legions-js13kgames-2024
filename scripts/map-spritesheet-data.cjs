850c68ee23cddbb781fe3a870bfadbdc127c0520
alszczep <al.szczepura@gmail.com>
2024-08-28 00:49:50 +0200

    Remove an unused sprite

--- scripts/map-spritesheet-data.cjs
+++ scripts/map-spritesheet-data.cjs
@@ -5,8 +5,7 @@ const parsedData = JSON.parse(data);
 
 const keysMap = {
     "player 0.aseprite": "ps", // player standing
-    "player 1.aseprite": "pah", // player attack
-    "player 2.aseprite": "pa", // player attack heavy
+    "player 1.aseprite": "pa", // player attack
     "knight 0.aseprite": "ks", // knight standing
     "knight 1.aseprite": "ka", // knight attack
     "rock.aseprite": "r", // rock
